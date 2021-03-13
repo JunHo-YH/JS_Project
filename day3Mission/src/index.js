3edabc94081af38973e3cf974c8f9b5b45879da6
JunHo <u_innovation@naver.com>
2021-03-14 00:28:01 +0900

    day3Mission

--- day3Mission/src/index.js
+++ day3Mission/src/index.js
@@ -1,28 +1,28 @@
-// import "./styles.css";
+import "./styles.css";
 
-// You're gonna need this
-
-const h1 = document.querySelector(".h1");
-
+const clockTitle = document.querySelector(".js-clock");
 
 function getTime() {
-  // Don't delete this.
-  const xmasDay = new Date("2021-12-24:00:00:00+0900").getTime();
-  const DateNowInfo = new Date();
-  const gap = xmasDay - DateNowInfo;
-
-  const day = Math.floor(gap / (1000 * 60 * 60 * 24));
-  const hour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const min = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
-  const sec = Math.floor((gap % (1000 * 60)) / 1000);
-
-  h1.innerHTML = `${day < 1 ? 0 : day}일 ${hour < 9 ? }시간 ${min}분 ${sec}초 남았습니다.`;
-
-
-
+  const xmasDay = new Date(`${new Date().getFullYear()}-12-24:00:00:00+0900`);
+  const now = new Date();
+  // This is in milisecondsx
+  const difference = new Date(xmasDay - now);
+  const secondsInMs = Math.floor(difference / 1000);
+  const minutesInMs = Math.floor(secondsInMs / 60);
+  const hoursInMs = Math.floor(minutesInMs / 60);
+  const days = Math.floor(hoursInMs / 24);
+  const seconds = secondsInMs % 60;
+  const minutes = minutesInMs % 60;
+  const hours = hoursInMs % 24;
+  const daysStr = `${days < 10 ? `0${days}` : days}d`;
+  const hoursStr = `${hours < 10 ? `0${hours}` : hours}h`;
+  const minutesStr = `${minutes < 10 ? `0${minutes}` : minutes}m `;
+  const secondsStr = `${seconds < 10 ? `0${seconds}` : seconds}s`;
+  clockTitle.innerHTML = `${daysStr} ${hoursStr} ${minutesStr} ${secondsStr}`;
 }
 
 function init() {
+  getTime();
   setInterval(getTime, 1000);
 }
 init();
